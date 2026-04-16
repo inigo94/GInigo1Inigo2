2f46e64d943eae7380106db4381a808f86a35607
Web <Web@A1EQUIPO25>
2026-04-16 09:24:15 +0200

    primer cambio

---
+++ proInigo1Inigo2/src/proinigo1inigo2/ProInigo1Inigo2.java
@@ -0,0 +1,12 @@
+
+package proinigo1inigo2;
+
+public class ProInigo1Inigo2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Primer cambio realizado por Usuario 1");
+        
+    }
+    
+}
