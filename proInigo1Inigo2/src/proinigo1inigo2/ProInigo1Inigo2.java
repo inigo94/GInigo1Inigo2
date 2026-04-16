e9b7cc7f4184ae751437466d3688fa52c14deaff
Web <Web@A1EQUIPO25>
2026-04-16 11:19:11 +0200

    Cambio de U1

--- proInigo1Inigo2/src/proinigo1inigo2/ProInigo1Inigo2.java
+++ proInigo1Inigo2/src/proinigo1inigo2/ProInigo1Inigo2.java
@@ -5,7 +5,7 @@ public class ProInigo1Inigo2 {
 
     public static void main(String[] args) {
         
-        System.out.println("Cambio por Usuario 2");
+        System.out.println("Linea del Usuario 1");
         
     }
     
