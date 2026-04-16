2d5a099c76d18593f4d612e51f2b15165f9d4b93
Web <Web@A1EQUIPO25>
2026-04-16 09:58:12 +0200

    tercer cambio

--- proInigo1Inigo2/src/proinigo1inigo2/ProInigo1Inigo2.java
+++ proInigo1Inigo2/src/proinigo1inigo2/ProInigo1Inigo2.java
@@ -5,8 +5,7 @@ public class ProInigo1Inigo2 {
 
     public static void main(String[] args) {
         
-        int version = 2;
-        System.out.println("Segunda validación: Versión " + version);
+        System.out.println("Tercer cambio");
         
     }
     
